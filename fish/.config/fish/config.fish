9a1064641b23ab963b7de6134ddf3e0e0d213f51
Shiron <contact@shiron.io>
2026-04-07 09:45:23 +0200

    feat(fish): Added startup time display in grey

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,12 @@
+set -l _start (date +%s%N)
+
 starship init fish | source
 fastfetch
 
 alias docker=podman
 
-
 # uv
 fish_add_path "/home/shiron/.local/bin"
+
+set -l _elapsed (math -s0 "($(date +%s%N) - $_start) / 1000000")
+echo (set_color brblack)"fish started in $_elapsed ms"(set_color normal)
