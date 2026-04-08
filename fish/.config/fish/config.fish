feb5ea34063def9f979665eaae88b55c36b3a92d
Shiron <contact@shiron.io>
2026-04-08 10:31:48 +0200

    feat(kde): Added KDE Plasma configs

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,3 +10,10 @@ fish_add_path "/home/shiron/.local/bin"
 
 set -l _elapsed (math -s0 "($(date +%s%N) - $_start) / 1000000")
 echo (set_color brblack)"fish started in $_elapsed ms"(set_color normal)
+
+# pnpm
+set -gx PNPM_HOME "/home/shiron/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
